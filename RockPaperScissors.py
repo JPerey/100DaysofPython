dcbdcfef4d675e92ad2d5e05bed977ace03346fd
jamjam.perey@gmail.com <jamjam.perey@gmail.com>
2024-05-16 22:03:35 -0700

    finished rock paper scissors game in like 5 minutes

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,19 +1,28 @@
 import random
 
-random_integer = random.randint(1,100)
-random_float = random.random()
-random_fiver = random_float*5
+player_choice = int(input("what do you choose? type 0 for rock, 1 for paper, or 2 for scissors."))
 
-print(f"random int: {random_integer}")
-print(f"random float: {random_float}")
-print(f"rnadom 1 to 5: {random_fiver}")
+print(f"player has chosen {player_choice}")
+rps_list = ["rock", "paper", "scissors"]
+npc_choice = random.randint(0,2)
+player_result = rps_list[player_choice]
+npc_result = rps_list[npc_choice]
 
-orig_list = [1, 2, 3, 4, 5]
+if player_choice == npc_choice:
+    both_choice = rps_list[player_choice]
+    print(f"tie game. player and npc picked {both_choice}")
+elif player_choice == 0 and npc_choice == 1:
+    print(f"npc wins. player: {player_result} || npc: {npc_result}")
+elif player_choice == 0 and npc_choice == 2:
+    print(f"player wins. player: {player_result} || npc: {npc_result}")
+elif player_choice == 1 and npc_choice == 0:
+    print(f"player wins. player: {player_result} || npc: {npc_result}")
+elif player_choice == 1 and npc_choice == 2:
+    print(f"npc wins. player: {player_result} || npc: {npc_result}")
+elif player_choice == 2 and npc_choice == 0:
+    print(f"npc wins. player: {player_result} || npc: {npc_result}")
+elif player_choice == 2 and npc_choice == 1:
+    print(f"player wins. player: {player_result} || npc: {npc_result}")
+else:
+    print("somehow wrong number detected")
 
-orig_list.append(6)
-
-print(f"appended list: {orig_list}")
-
-orig_list.extend([7, 8, 9])
-
-print(f"extended list: {orig_list}")
