5515e80e926748fe8f73ccc3ebcd01e0dfa625c9
jamjam.perey@gmail.com <jamjam.perey@gmail.com>
2024-05-19 10:52:20 -0700

    finished fizzbuzz problem

--- Day5.py
+++ Day5.py
@@ -52,3 +52,14 @@ for num in range(2,target+1,2):
   total += num
 
 print(total)
+
+# FizzBuzz problem
+for num in range(1,101):
+  if num%3 == 0 and num%5== 0:
+    print("FizzBuzz")
+  elif num%5 == 0:
+    print("Buzz")
+  elif num%3 == 0:
+    print("Fizz")
+  else:
+    print(num)
