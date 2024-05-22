a8e334ab98438fdf73dc2d4134fb6b2e62a5ffb2
jamjam.perey@gmail.com <jamjam.perey@gmail.com>
2024-05-21 20:56:27 -0700

    beginning my coding practice for Day 6

---
+++ Day6.py
@@ -0,0 +1,13 @@
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+# Creating a function
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+# running the function
+
+my_function()
