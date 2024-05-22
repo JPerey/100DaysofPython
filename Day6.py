8d7438b5605de00e4c6bb9c31f144afff805c2fa
jamjam.perey@gmail.com <jamjam.perey@gmail.com>
2024-05-21 22:16:42 -0700

    reeborg example

--- Day6.py
+++ Day6.py
@@ -11,3 +11,33 @@ def my_function():
 # running the function
 
 my_function()
+
+# Reeborgs example function
+'''
+def turn_right ( ):
+    turn_left()
+    turn_left()
+    turn_left()
+
+def move_two ( ):
+    move()
+    move()
+
+def get_news():
+    turn_left()
+    move()
+    turn_right()
+    move_two()
+    turn_left()
+    move()
+    turn_right()
+    move_two()
+    turn_left()
+    move()
+    turn_right()
+    move_two()
+    
+    
+get_news()
+take()
+'''
