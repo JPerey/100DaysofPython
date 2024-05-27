43ed66a2f790f75a48b74c330aa014ae67d1c476
jamjam.perey@gmail.com <jamjam.perey@gmail.com>
2024-05-27 08:21:17 -0700

    updating day8 practice file

--- Day8.py
+++ Day8.py
@@ -14,3 +14,24 @@ def greet_with_name(name):
     print(f"It's glad to hear from you {name}!")
 
 greet_with_name("Jam")
+
+# paint coverage exercise
+"""
+# Write your code below this line 👇
+import math
+def paint_calc(height, width, cover):
+  area_square = height * width
+  num_cans = math.ceil(area_square/ cover)
+  print(f"You'll need {num_cans} cans of paint.")
+
+
+
+# Write your code above this line 👆
+# Define a function called paint_calc() so the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input()) # Height of wall (m)
+test_w = int(input()) # Width of wall (m)
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+"""
