5c01586cb102af4a17ca2eedb43bc9ee58496d3b
jamjam.perey@gmail.com <jamjam.perey@gmail.com>
2024-05-27 07:56:18 -0700

    beginning code for Day 8

---
+++ Day8.py
@@ -0,0 +1,16 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+    print("This is greeting 1!")
+    print("This is greeting 2!")
+    print("This is greeting 3!")
+
+greet()
+
+def greet_with_name(name):
+    print(f"It's glad to hear from you {name}!")
+
+greet_with_name("Jam")
