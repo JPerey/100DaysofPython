9de6097b48b6a51fc856370b0d3a5c6e433a7502
jamjam.perey@gmail.com <jamjam.perey@gmail.com>
2024-05-28 20:56:09 -0700

    finished nesting practice

--- Day9.py
+++ Day9.py
@@ -36,3 +36,50 @@ for student in student_scores:
 # 🚨 Don't change the code below 👇
 print(student_grades)
 '''
+
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+Travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+}
+
+# nesting dictionary practice
+
+'''
+country = input() # Add country name
+visits = int(input()) # Number of visits
+list_of_cities = eval(input()) # create list from formatted string
+
+travel_log = [
+  {
+    "country": "France",
+    "visits": 12,
+    "cities": ["Paris", "Lille", "Dijon"]
+  },
+  {
+    "country": "Germany",
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Stuttgart"]
+  },
+]
+# Do NOT change the code above 👆
+
+# TODO: Write the function that will allow new countries
+# to be added to the travel_log. 
+def add_new_country(country,times_visited,cities_visited):
+  new_log_entry = {
+    "country": country,
+    "visits": times_visited,
+    "cities": cities_visited,
+  }
+  travel_log.append(new_log_entry)
+  
+
+# Do not change the code below 👇
+add_new_country(country, visits, list_of_cities)
+print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+print(f"My favourite city was {travel_log[2]['cities'][0]}.")
+'''
