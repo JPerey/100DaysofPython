3e0d8fa6d0ee3f7560761b6c5e208238a99fb719
jamjam.perey@gmail.com <jamjam.perey@gmail.com>
2024-06-11 23:28:26 -0700

    understanding the difference between local and global scope

---
+++ Day12.py
@@ -0,0 +1,3 @@
+#Global vs Local Scope
+# Global scope is the scope in which all functions are in, and even the starting code. Any variable created and accessed by all functions and all parts of the program are in the global scope.
+#local scope is a scope created by functions and the indents the functions create. Anything within a function is seperate from the global scope, and in that space, it is considered local scope.
