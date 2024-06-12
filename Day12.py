b3c45fc224cce4f47b9a59eab74390e6784f7b48
jamjam.perey@gmail.com <jamjam.perey@gmail.com>
2024-06-11 23:53:27 -0700

    completed guess the number program for day 12

--- Day12.py
+++ Day12.py
@@ -1,3 +1,5 @@
 #Global vs Local Scope
 # Global scope is the scope in which all functions are in, and even the starting code. Any variable created and accessed by all functions and all parts of the program are in the global scope.
 #local scope is a scope created by functions and the indents the functions create. Anything within a function is seperate from the global scope, and in that space, it is considered local scope.
+# use the keyword 'global' to modify a global variable inside a local space. THIS IS NOT RECOMMENDED, as it is prone to creating bugs
+#instead, you would want to return a value changed from a function to the global variable
