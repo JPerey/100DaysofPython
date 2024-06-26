6f8ecb957aa1252303de0822fc0ba1c8611ce8b2
jamjam.perey@gmail.com <jamjam.perey@gmail.com>
2024-06-25 22:16:08 -0700

    made changes for Day 16

---
+++ Day16/Day16.py
@@ -0,0 +1,14 @@
+import turtle
+import another_module
+
+print(another_module.another_variable)
+
+timmy = turtle.Turtle()
+my_screen = turtle.Screen()
+print(timmy)
+
+timmy.shape("turtle")
+timmy.color("blue")
+timmy.forward(100)
+print(my_screen.canvheight)
+my_screen.exitonclick()
