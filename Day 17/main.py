92d60c4655cde9f16c6939ab0795ed65360da4e1
jamjam.perey@gmail.com <jamjam.perey@gmail.com>
2024-07-01 21:01:23 -0700

    beginning class practice day 17

---
+++ Day 17/main.py
@@ -0,0 +1,11 @@
+class User: #classes in Python is in PascalCase
+    
+    def __init__(self) -> None: #used to initialize attributes
+        
+        pass
+
+user_1 = User()
+user_1.id = "0001"
+user_1.username = "coolguy"
+
+print(user_1.username)
