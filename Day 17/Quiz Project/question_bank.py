c598529f65e112185f72e7336d94a12e6bbc4cbe
jamjam.perey@gmail.com <jamjam.perey@gmail.com>
2024-07-06 18:24:58 -0700

    working on question bank and main.py for object quiz project

--- Day 17/Quiz Project/question_bank.py
+++ Day 17/Quiz Project/question_bank.py
@@ -1,5 +1,11 @@
 import pip._vendor.requests as requests
-def Get_Quiz():
-    quiz_bank = requests.get("https://opentdb.com/api.php?amount=10&difficulty=medium&type=boolean")
 
-    return quiz_bank
+class Question_Bank:
+
+    def Get_Quiz(self):
+
+        def __init__(self):
+            pass
+
+        quiz_bank = requests.get("https://opentdb.com/api.php?amount=10&difficulty=medium&type=boolean")
+        print(f"quiz bank = {quiz_bank}")
